914e8f0319918913a139b426d6e5efd6c31d088a
Tolu Solomon <tolusolomon26.sts@gmail.com>
2025-03-29 17:56:29 -0400

    updates

--- cmd/main.go
+++ cmd/main.go
@@ -1,30 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"text/template"
+
+	"github.com/Toluxpersia/tolu.com/pkg"
 )
 
 func main() {
+	pkg.Connect()
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html")
-		fmt.Fprintln(w, `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Hello, World</title>
-</head>
-<body>
-<h1>Hello, World!</h1>
-<p>Welcome to your first Go web server.</p>
-</body>
-</html>
-`)
-
-	})
+	mux.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":8080", mux)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("index.html"))
+
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, "Error rendering template", http.StatusInternalServerError)
+	}
 }
